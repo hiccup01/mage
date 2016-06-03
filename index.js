fa2ffffd8143d83516e7a0252ff478f195575466
hiccup01 <hiccup@hiccup01.com>
2016-06-04 10:34:43 +1200

    The beginnings!

---
+++ index.js
@@ -0,0 +1,22 @@
+// mage
+function loadImg(num, settings, callback) {
+var defaults = {
+	"location":function() {
+		document.body.getElementById('images');
+	},
+	"tag":"image",
+	"start":1,
+	"imagetag":"innerimage",
+	"format":".png"
+}
+var types = ["location", "tag", "start", "imagetag", "format"];
+for (var i = 0; i < types.length; i++) {
+	if (typeof settings.(types[i]) == undefined) {
+		settings.(types[i]) = defaults.(types[i]);
+	}
+}
+
+for (var i = 0; i < num; i++) {
+	(settings.location).insertAdjacentHTML("afterBegin","<div id=\"" + settings.tag + "\"><img id=\"" + settings.imagetag + "\" src=\"./photos/photo" + (i + settings.start) + settings.format + "\"></div>\n");
+}
+	  	}
