130b386a735a199a320d45b3cfa5e4713921319c
hiccup01 <hiccup@hiccup01.com>
2016-06-04 13:05:35 +1200

    Fix up some things

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // mage
-function loadImg(num, settings, callback) {
+function mage(num, settings, callback) {
 var defaults = {
 	"location":function() {
 		document.body.getElementById('images');
@@ -11,12 +11,11 @@ var defaults = {
 }
 var types = ["location", "tag", "start", "imagetag", "format"];
 for (var i = 0; i < types.length; i++) {
-	if (typeof settings.(types[i]) == undefined) {
-		settings.(types[i]) = defaults.(types[i]);
+	if (typeof settings[(types[i])] == undefined) {
+		settings[(types[i])] = defaults[(types[i])];
 	}
 }
-
 for (var i = 0; i < num; i++) {
 	(settings.location).insertAdjacentHTML("afterBegin","<div id=\"" + settings.tag + "\"><img id=\"" + settings.imagetag + "\" src=\"./photos/photo" + (i + settings.start) + settings.format + "\"></div>\n");
 }
-	  	}
+}
