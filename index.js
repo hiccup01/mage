d99d6fffea4dce9a65c61ed4939537ab3c7b33a5
hiccup01 <hiccup@hiccup01.com>
2016-06-07 07:37:10 +1200

    testing

--- index.js
+++ index.js
@@ -6,16 +6,19 @@ var defaults = {
 	},
 	"tag":"image",
 	"start":1,
+	"path":"./images/image",
 	"imagetag":"innerimage",
 	"format":".png"
 };
-var types = ["location", "tag", "start", "imagetag", "format"];
+var types = ["location", "tag", "start", "path", "imagetag", "format"];
 for (var i = 0; i < types.length; i++) {
 	if (typeof settings[(types[i])] == undefined) {
 		settings[(types[i])] = defaults[(types[i])];
 	}
 }
 for (var i = 0; i < num; i++) {
-	(settings.location).insertAdjacentHTML("afterBegin","<div id=\"" + settings.tag + "\"><img id=\"" + settings.imagetag + "\" src=\"./photos/photo" + (i + settings.start) + settings.format + "\"></div>\n");
+	(settings.location).insertAdjacentHTML("afterBegin","<div id=\"" + settings.tag + "\"><img id=\"" + settings.imagetag + "\" src=\"" + settings.path + (i + settings.start) + settings.format + "\"></div>\n");
 }
+callback(settings.location);
 }
+document.addEventListener("load", mage(7,{"location":function(){document.getElementsByClassName("contentmain")[0];},"tag": "photo", "start":1, })); 
